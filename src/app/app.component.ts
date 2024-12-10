ad222684014e2b5bd7b629600ad5ae917cbc4fab
homerduc <alexandre.duc00@orange.fr>
2024-12-10 11:16:59 +0100

    Ajout de la première version du footer et de la navbar (mat-toolbar)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import {TableauComponent} from './utilisateur/tableau/tableau.component';
 import {MatTableModule} from '@angular/material/table';
 import {UserFormComponent} from './utilisateur/user-form/user-form.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, HttpClientModule, TableauComponent, MatTableModule, UserFormComponent],
+  imports: [RouterOutlet, FormsModule, HttpClientModule, TableauComponent, MatTableModule, UserFormComponent, MatToolbarModule, MatButtonModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
